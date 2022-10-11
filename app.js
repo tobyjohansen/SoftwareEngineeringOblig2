9c8e4251f1e62adcbf11ff41361edca4eee4e9a2
tobyjohansen <tobias.p.johansen@outlook.com>
2022-10-11 20:01:32 +0200

    changed year % 400 = 1 | from 0

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const isLeapYear = (year) => {
-  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 1) {
     return true;
   }
   return false;
